cfa3037de4e0ba2aa45c03a18c850218bbb025ca
Reza Ashena <Ashena.reza@gmail.com>
2025-07-10 09:56:41 -0400

    Add ML.NET workflow for sentiment analysis
    Implemented a new machine learning workflow using ML.NET.
    Updated `using` directives, created an `MLContext` instance,
    and added methods to load data and build a logistic regression
    model for sentiment analysis. This enhances the application's
    capability to process and predict sentiment from text data.

--- WebsiteCommentPredictor/Program.cs
+++ WebsiteCommentPredictor/Program.cs
@@ -1,4 +1,25 @@
 ﻿using Microsoft.ML;
-using static Microsoft.ML.DatabaseLoaderCatalog;
+using WebsiteCommentPredictor;
+using static Microsoft.ML.DataOperationsCatalog;
 
 string _yelpDataPath = Path.Combine(Environment.CurrentDirectory, "Data", "yelp_labelled.txt");
+MLContext _mlContext = new MLContext();
+TrainTestData splitDataView = LoadData(_mlContext);
+ITransformer model = BuildAndTrainModel(_mlContext, splitDataView.TrainSet);
+
+TrainTestData LoadData(MLContext mlContext)
+{
+    IDataView dataView = mlContext.Data.LoadFromTextFile<SentimentData>(_yelpDataPath);
+    TrainTestData splitDataView = mlContext.Data.TrainTestSplit(dataView, 0.2);
+    return splitDataView;
+
+}
+
+ITransformer BuildAndTrainModel(MLContext mlContext,IDataView splitTrainSet)
+{
+    var estimator = mlContext.Transforms.Text.FeaturizeText("Features", nameof(SentimentData.SentimentText))
+        .Append(mlContext.BinaryClassification.Trainers.SdcaLogisticRegression("Label", "Features"));
+    var model =estimator.Fit(splitTrainSet);
+    return model;
+}
+
